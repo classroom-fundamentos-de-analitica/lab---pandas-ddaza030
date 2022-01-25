ab5aa96a63a09904f7c5f94135e5962ccc9dfb8a
Daniel Daza <80338410+ddaza030@users.noreply.github.com>
2022-01-25 14:13:26 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -156,7 +156,7 @@ def pregunta_09():
     38   38   E    1  1999-09-28  1999
     39   39   E    5  1998-01-26  1998
 
-    """}
+    """
     
     
     return sorted(map(may , list(tbl1._c4.unique())))
